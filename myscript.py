03459b19ecbf6d978c95a513ce659d39f0b7db7a
Harsh Shah <hash.neon@gmail.com>
2013-05-23 20:41:18 +0530

    second commit

--- myscript.py
+++ myscript.py
@@ -1,15 +1,6 @@
-import numpy
-import sys
-
-def main(parameters):
-    numpy.random.seed(parameters["seed"])
-    distr = getattr(numpy.random, parameters["distr"])
-    data = distr(size=parameters["n"])
-    output_file = "output"
-    numpy.savetxt(output_file, data)
-
-parameter_file = sys.argv[1]
-parameters = {}
-execfile(parameter_file, parameters) # this way of reading parameters
-                                     # is not necessarily recommended
-main(parameters)
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+printf("\nFirst Code\n",);
+return 0;
+}

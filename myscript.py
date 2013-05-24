1f1aafaabbae0e2d70851839a0ae4d77a370ad99
Harsh Shah <hash.neon@gmail.com>
2013-05-24 15:12:11 +0530

    thrid  commit

--- myscript.py
+++ myscript.py
@@ -1,5 +1,15 @@
-#include<stdio.h>
-int main(int argc, char *argv[])
-{
-return 0;
-}
+import numpy
+import sys
+
+def main(parameters):
+    numpy.random.seed(parameters["seed"])
+    distr = getattr(numpy.random, parameters["distr"])
+    data = distr(size=parameters["n"])
+    output_file = "output"
+    numpy.savetxt(output_file, data)
+
+parameter_file = sys.argv[1]
+parameters = {}
+execfile(parameter_file, parameters) # this way of reading parameters
+                                     # is not necessarily recommended
+main(parameters)
